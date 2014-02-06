2e0a93a520f34035af563462608d43456d66b11c
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-06 15:10:46 -0600

    chg to download image in advance and pass a file:// url to devstack

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 distro=`lsb_release -s -c`
 sudo cat > /etc/apt/sources.list.d/linaro-overlay.list << EOF
 deb http://repo.linaro.org/ubuntu/linaro-overlay ${distro} main
@@ -9,6 +11,10 @@ sudo apt-key add linarorepo.key
 sudo apt-get update
 
 sudo apt-get -y install qemu-system libvirt-bin python-libvirt bridge-utils pm-utils
+
+pushd /tmp
+wget http://people.linaro.org/~clark.laughlin/ubuntu-cloud-guest-patched/saucy-server-cloudimg-armhf-update.tgz
+popd
 
 ./stack.sh
 
