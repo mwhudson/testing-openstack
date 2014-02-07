10b81be9d824164f844e714bc6ab83ff4cf16046
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-07 11:50:40 -0600

    remove key after downloading/adding

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ EOF
 
 wget http://repo.linaro.org/ubuntu/linarorepo.key
 sudo apt-key add linarorepo.key
+rm linarorepo.key
 sudo apt-get update
 
 sudo apt-get -y install qemu-system libvirt-bin python-libvirt bridge-utils pm-utils
