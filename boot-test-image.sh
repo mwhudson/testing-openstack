c2055cff1f2902f5ada49f9fc617775a53887f38
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-10 10:47:54 -0600

    initial version

---
+++ boot-test-image.sh
@@ -0,0 +1,8 @@
+DESIRED_FLAVOR=m1.micro
+DESIRED_IMAGE_PREFIX=saucy-server
+
+image_uuid=`glance image-list | awk '/'${DESIRED_IMAGE_PREFIX}'.*ami/{print $2}'`
+flavor_id=`nova flavor-list | awk '/'${DESIRED_FLAVOR}'/{print $2}'`
+
+nova boot linaro-test -image "${image_uuid}" --flavor ${flavor_id}
+
