36dc91542e3d6fcf6bbe21e15dcac3bb3abe3acb
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-12 11:30:04 -0600

    added configuration of eth1, removed download of cloud-guest

--- install.sh
+++ install.sh
@@ -6,16 +6,18 @@ deb http://repo.linaro.org/ubuntu/linaro-overlay ${distro} main
 deb-src http://repo.linaro.org/ubuntu/linaro-overlay ${distro} main
 EOF
 
+sudo cat >> /etc/network/interfaces << EOF
+
+auto eth1
+iface eth1 inet dhcp
+EOF
+
 wget http://repo.linaro.org/ubuntu/linarorepo.key
 sudo apt-key add linarorepo.key
 rm linarorepo.key
 sudo apt-get update
 
 sudo apt-get -y install qemu-system libvirt-bin python-libvirt bridge-utils pm-utils
-
-#pushd /tmp
-#wget http://people.linaro.org/~clark.laughlin/ubuntu-cloud-guest-patched/saucy-server-cloudimg-armhf-update.tgz
-#popd
 
 ./stack.sh | tee install.log
 
