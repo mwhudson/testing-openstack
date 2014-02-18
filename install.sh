3d6d5ad8382c4990976fda816ff40f1091561820
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-18 17:02:08 -0600

    initial version, moved installation to external script to share between devstack-exercises.yaml and tempest.yaml

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+# store off the current directory and create a temp directory
+export TESTDIR=`pwd`
+export TMPDIR=${TESTDIR}/tmp
+mkdir -p ${TMPDIR}
+
+# write out Linaro tools PPA configuration file
 distro=`lsb_release -s -c`
 sudo cat > /etc/apt/sources.list.d/linaro-overlay.list << EOF
 deb http://repo.linaro.org/ubuntu/linaro-overlay ${distro} main
 deb-src http://repo.linaro.org/ubuntu/linaro-overlay ${distro} main
 EOF
 
-sudo cat >> /etc/network/interfaces << EOF
-
-auto eth1
-iface eth1 inet dhcp
-EOF
-
+# import repo key
 wget http://repo.linaro.org/ubuntu/linarorepo.key
 sudo apt-key add linarorepo.key
 rm linarorepo.key
 sudo apt-get update
 
 # install some dependencies
-sudo apt-get -y install qemu-system libvirt-bin python-libvirt bridge-utils pm-utils
+sudo apt-get -y install qemu-system libvirt-bin python-libvirt
 
 # get devstack
 git clone git://git.linaro.org/people/clark.laughlin/devstack.git
@@ -26,9 +27,11 @@ cp local.sh ./devstack
 cp local.conf ./devstack
 cp boot-test-image.sh ./devstack
 
-pushd devstack
+# configure workarounds for linaro images
+./workarounds.sh
+
+# setup devstack
+ devstack
 ./stack.sh | tee install.log
-popd
-
-
+cd ${TESTDIR}
 
