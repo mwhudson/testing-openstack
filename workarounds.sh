db914c9db5494d1a21dc422a7513d4414acc7951
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-19 12:30:03 -0600

    workaround for https://bugs.launchpad.net/ubuntu/+source/python-keystoneclient/+bug/1242992

--- workarounds.sh
+++ workarounds.sh
@@ -4,3 +4,6 @@
 echo "updating /etc/network/interfaces"
 sed --in-place 's/source-directory/#source-directory/' /etc/network/interfaces
 
+# Workaround for https://bugs.launchpad.net/ubuntu/+source/python-keystoneclient/+bug/1242992:
+sudo apt-get -y remove python-keyring
+sudo apt-get -y autoremove
