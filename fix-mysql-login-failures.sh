32f2c798dacf776d44fc0f67446b991cc5520adc
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-21 22:04:04 -0600

    just login and set password

--- fix-mysql-login-failures.sh
+++ fix-mysql-login-failures.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sudo service mysql stop
-sudo /usr/sbin/mysqld --skip-grant-tables --skip-networking &
-
-sleep 15
-
 mysql -u root <<EOF
 USE mysql;
 UPDATE user SET Password = PASSWORD('password')
@@ -12,6 +7,5 @@ WHERE Host = 'localhost' AND User = 'root';
 FLUSH PRIVILEGES;
 EOF
 
-sudo pkill mysqld
-sudo service mysql start
+sudo /etc/init.d/mysql restart
 
