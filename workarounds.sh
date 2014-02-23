0f86b59e81178788bea416d724c2aeb66b4fc2bf
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-22 21:14:05 -0600

    just uninstall mysql completely

--- workarounds.sh
+++ workarounds.sh
@@ -8,5 +8,12 @@ sed --in-place 's/source-directory/#source-directory/' /etc/network/interfaces
 sudo apt-get -y remove python-keyring
 sudo apt-get -y autoremove
 
+# uninstall mysql so devstack can reinstall it
+sudo apt-get -y remove mysql-server
+mysql_data_dir=/var/lib/mysql
+if [ -d "${mysql_data_dir}" ]; then
+   sudo rm -rf ${mysql_data_dir}
+fi
+
 # For some reason, mysql on Linaro images does not allow root login correctly
-bash -x ./fix-mysql-login-failures.sh
+#bash -x ./fix-mysql-login-failures.sh
