13c3b2e8996d05e8d6bf2ec8b00b30c2fefddb81
Clark Laughlin <clark.laughlin@linaro.org>
2014-02-24 10:28:10 -0600

    added some additional messages

--- local.sh
+++ local.sh
@@ -16,12 +16,14 @@ source $TOP_DIR/lib/nova
 source $TOP_DIR/openrc admin admin
 
 # Create new flavor to use for running exercises, if not already present
+echo "Create new Linaro test flavor"
 MI_NAME=m1.linarotest
 if [[ -z $(nova flavor-list | grep $MI_NAME) ]]; then
     nova flavor-create $MI_NAME 7 512 2 1 
 fi
 
 # create SSH key if not present
+echo "Create Linaro SSH keypair"
 KEYPAIR_NAME=LinaroKey
 if [[ -z $(nova keypair-list | grep $KEYPAIR_NAME) ]]; then
     nova keypair-add ${KEYPAIR_NAME} > ${TOP_DIR}/${KEYPAIR_NAME}.pem
@@ -29,8 +31,11 @@ if [[ -z $(nova keypair-list | grep $KEYPAIR_NAME) ]]; then
 fi
 
 # set property of image to run as virt model
+echo "Update image with properties required to run as a mach-virt guest"
 IMAGE_UUID=`glance image-list | awk '/saucy-server.*ami/{print $2}'`
 glance image-update $IMAGE_UUID --property hw_machine_type=virt
 glance image-update $IMAGE_UUID --property hw_cdrom_bus=virtio
 glance image-update $IMAGE_UUID --property os_command_line='root=LABEL=cloudimg-rootfs rw rootwait console=ttyAMA0'
 
+echo "Host List:"
+nova host-list
