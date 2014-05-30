5ea5b23497dadcf295363fa072e6a8838b5b7396
Michael Hudson-Doyle <michael.hudson@linaro.org>
2014-05-30 13:02:14 +1200

    Some local.sh changes.

--- local.sh
+++ local.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 NOVA_CONF=/etc/nova/nova.conf
 
 # keep track of the devstack directory
@@ -15,13 +16,6 @@ source $TOP_DIR/lib/nova
 # Get OpenStack admin auth
 source $TOP_DIR/openrc admin admin
 
-# Create new flavor to use for running exercises, if not already present
-echo "Create new Linaro test flavor"
-MI_NAME=m1.linarotest
-if [[ -z $(nova flavor-list | grep $MI_NAME) ]]; then
-    nova flavor-create $MI_NAME 7 512 2 1 
-fi
-
 # create SSH key if not present
 echo "Create Linaro SSH keypair"
 KEY_NAME=LinaroKey
@@ -32,10 +26,10 @@ fi
 
 # set property of image to run as virt model
 echo "Update image with properties required to run as a mach-virt guest"
-IMAGE_UUID=`glance image-list | awk '/saucy-server.*ami/{print $2}'`
+IMAGE_UUID=`glance image-list | awk '/linaro.*ami/{print $2}'`
 glance image-update $IMAGE_UUID --property hw_machine_type=virt
 glance image-update $IMAGE_UUID --property hw_cdrom_bus=virtio
-glance image-update $IMAGE_UUID --property os_command_line='root=LABEL=cloudimg-rootfs rw rootwait console=ttyAMA0'
+glance image-update $IMAGE_UUID --property os_command_line='root=/dev/vdb rw rootwait console=ttyAMA0'
 
 echo "Host List:"
 nova host-list
